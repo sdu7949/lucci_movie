a65dce2309a9523bd9d6a7f774272b817db80bb3
username <user@email.mail>
2019-12-13 13:51:08 +0900

    Infinite v0.2

--- src/Routes/Notice/Scroll.js
+++ src/Routes/Notice/Scroll.js
@@ -64,6 +64,30 @@ export default class Scroll extends React.Component {
                  한층 진화된 예측 불가능한 게임 속에서 현실 세계로 돌아오기 위해 목숨을 건 미션을 시작하는데......`, 
                 image: `https://movie-phinf.pstatic.net/20191120_230/15742165480563skun_JPEG/movie_image.jpg` 
             },
+            { 
+                id: 4, 
+                name: '쥬만지', 
+                contents: `진화된 게임, 주어진 목숨은 3개! 사라진 친구를 찾아 반드시 탈출하라!
+                쥬만지 게임으로부터 가까스로 탈출해 평화로운 일상을 보내던 스펜서와 친구들.
+                 어느 날, 스펜서는 망가진 쥬만지 게임 속으로 사라지고
+                 마사와 베서니, 프리지 역시 그를 찾기 위해 다시 들어가기로 결심한다.
+                 그리고 우연히 게임에 합류하게 된 새로운 멤버, 스펜서의 할아버지 에디와 마일로.
+                 위험천만한 게임 속으로 들어가게 된 이들은 정글부터 설산, 사막까지
+                 한층 진화된 예측 불가능한 게임 속에서 현실 세계로 돌아오기 위해 목숨을 건 미션을 시작하는데......`, 
+                image: `https://movie-phinf.pstatic.net/20191120_230/15742165480563skun_JPEG/movie_image.jpg` 
+            },
+            { 
+                id: 5, 
+                name: '쥬만지', 
+                contents: `진화된 게임, 주어진 목숨은 3개! 사라진 친구를 찾아 반드시 탈출하라!
+                쥬만지 게임으로부터 가까스로 탈출해 평화로운 일상을 보내던 스펜서와 친구들.
+                 어느 날, 스펜서는 망가진 쥬만지 게임 속으로 사라지고
+                 마사와 베서니, 프리지 역시 그를 찾기 위해 다시 들어가기로 결심한다.
+                 그리고 우연히 게임에 합류하게 된 새로운 멤버, 스펜서의 할아버지 에디와 마일로.
+                 위험천만한 게임 속으로 들어가게 된 이들은 정글부터 설산, 사막까지
+                 한층 진화된 예측 불가능한 게임 속에서 현실 세계로 돌아오기 위해 목숨을 건 미션을 시작하는데......`, 
+                image: `https://movie-phinf.pstatic.net/20191120_230/15742165480563skun_JPEG/movie_image.jpg` 
+            },
         ])
 
     };
@@ -124,7 +148,7 @@ export default class Scroll extends React.Component {
                             <Titles>{i.name}</Titles>
                             <Contents>{i.contents}</Contents>
                         </div>
-                        <Images><img src={i.image} height="200px"></img></Images>
+                        <Images><img src={i.image} height="200px" alt="imagezx"></img></Images>
                     </Container>
                 ))}
             </InfiniteScroll>
